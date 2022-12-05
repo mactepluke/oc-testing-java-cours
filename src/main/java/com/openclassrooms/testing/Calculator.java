80c3c816039094f8e1d29130affbd7e0bdb141f1
Jean-Luc METZ <116441043+mactepluke@users.noreply.github.com>
2022-12-05 05:17:39 +0100

    Ajouts Luc

--- src/main/java/com/openclassrooms/testing/Calculator.java
+++ src/main/java/com/openclassrooms/testing/Calculator.java
@@ -1,9 +1,33 @@
 package com.openclassrooms.testing;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Calculator {
 
 	public int add(int a, int b) {
 		return a + b;
 	}
 
+	public int multiply(int a, int b) {
+		return a * b;
+	}
+
+	public Set<Integer> digitsSet(int number) {
+		Set<Integer> integers = new HashSet<Integer>();
+		String numberString = String.valueOf(number);
+		
+		for (int i = 0; i < numberString.length(); i++) {
+			integers.add(Integer.parseInt(numberString, i, i+1, 10));
+		}
+		return integers;
+	}
+
+	public double add(double a, double b) {
+		return a + b;
+	}
+
+	public double multiply(double a, double b) {
+		return a * b;
+	}
 }
